5de9fcb5a39e4cc3ce92eff62e592bc23412b2a5
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-06 13:54:59 +0800

    Update image.php

--- scripts/image.php
+++ scripts/image.php
@@ -3,6 +3,12 @@
 class image {
 
     public static function process_upload($raw) {
-        
+        include_once APP_PATH . "/framework/PHPExif/lib/PHPExif/Reader/Reader.php";
+
+        // reader with Native adapter
+        $reader = \PHPExif\Reader\Reader::factory(\PHPExif\Enum\ReaderType::NATIVE);
+        $exif = $reader->read($raw);
+
+        breakpoint($exif);
     }
 }
